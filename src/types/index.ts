879eccc7e97e4e533add275abd0e668cdc98d32e
Marina Sysoliatina <stereogam@yandex.ru>
2024-05-30 12:42:47 +0200

    added constructor describes and review fixes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { IEvents } from '../components/base/events'
+
 //интерфейс для данных товара
 export interface IItem {
     category: string;
@@ -62,3 +64,59 @@ export type IOrderSuccessPopup = Pick<IOrder, 'total'>;
 //тип данных для отображения карточки товара в корзине
 export type IPopupItemInBacket = Pick<IItem, 'title' | 'price'>
 
+
+//типы данных для ИНТЕРФЕЙСА ПРЕДСТАВЛЕНИЯ
+
+//*Абстрактный!* класс  служит шаблоном для создания компонентов, которые будут работать с элементами DOM. 
+export interface Component {
+    toggleClass(element: HTMLElement, className: string, force?: boolean): void;
+    setText(element: HTMLElement, value: unknown): void;
+    setDisabled(element: HTMLElement, state: boolean): void;
+    setHidden(element: HTMLElement): void;
+    setVisible(element: HTMLElement): void;
+    render(): HTMLElement;
+}
+
+//Отвечает за отображение карточек на главной странице, в модальном окне просмотра карточки, в корзине. 
+export interface Card extends Component {
+    setData(cardData: IItem): void;
+    getCard(card: object): HTMLElement;
+    getIdCard(card: object): string;
+}
+
+//*ОБЩИЙ* класс модального окна.
+export interface ModalPopUp {
+    modal: HTMLElement;
+    event: IEvents; 
+    openPopupWindow(): void;
+    closePopupWindow(): void;
+    addContentToPopupWindow(template: HTMLElement): void;
+}
+ 
+//Расширяет класс `ModalPopUp`. Отвечает за отображениие выбранных товаров в корзине
+export interface BasketPopUp extends ModalPopUp {
+    basketList: HTMLElement;
+    submitButton: HTMLButtonElement;
+    totalSum: HTMLElement;
+    showCards(card: HTMLElement): void;
+}
+
+//Расширяет класс `ModalPopUp`. Отвечает за работу с полями ввода формы. 
+export interface FormsPopUp extends ModalPopUp {
+    submitButton: HTMLButtonElement;
+    formName: string;
+    form: HTMLElement;
+    inputs: [];
+    errors: Record<string, HTMLElement>;
+    setValid(isValid: boolean): void;
+    getInputValues(): Record<string, string>;
+    setError(data: { field: string, value: string, validInformation: string }): void;
+    get (form: HTMLElement): HTMLElement;
+}
+
+//Расширяет класс `ModalPopUp`. Отвечает за отображение модального окна после успешного завершения заказа. 
+export interface SucessOrderPopup extends ModalPopUp {
+    submitButton: HTMLElement;
+    totalSum: HTMLElement;
+    goBackToMainPage(): void;
+}
